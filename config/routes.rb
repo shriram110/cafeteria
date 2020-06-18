41c682e489701bf0304ffa6a8ed6e1452f376f16
shriram110 <shrirambharadwajsb@gmail.com>
2020-06-18 17:07:18 +0530

    add new files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :menus
+  resources :users do
+    member do
+      get :confirm_email
+    end
+  end
+  resources :cartitems
+  resources :orders
+  resources :userhome
+  resources :pendingorders
+  resources :menuitems
+  get "orders/report", to: "orders#report"
+  get "menuitems/hold", to: "menuitems#hold"
+  patch "users/select", to: "users#select"
+  patch "users/details", to: "users#details"
+  get "users/checkers", to: "users#show", as: :checkers
+  post "orders/hold", to: "orders#hold"
+  post "orders/select", to: "orders#select"
+  get "/" => "home#index"
+  delete "cartitems", to: "cartitems#deleteall"
+  get "/sing-in" => "sessions#new", as: :new_sessions
+  post "/sign_in", to: "sessions#create", as: :sessions
+  delete "/signout" => "sessions#destroy", as: :destroy_session
 end
