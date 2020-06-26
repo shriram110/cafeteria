c86adf2a227f02d32cfd327bfdafb0fc363307b2
shriram110 <shrirambharadwajsb@gmail.com>
2020-06-26 09:37:22 +0530

    css files updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,6 @@ class ApplicationController < ActionController::Base
 
   def current_user
     return @current_user if @current_user
-
     current_user_id = session[:current_user_id]
     if current_user_id
       if User.find(current_user_id).role == "Bill Checker"
